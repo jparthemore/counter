f78185ab1182b187ca0fbe694caa5bd6d6a498f9
Jan Parthemore <janparthemore@gmail.com>
2017-06-02 19:19:55 -0400

    constructed incrementing counter module as well as the associated html for the user interface. Removed that horrible green background

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
-console.log('yayyyyyyyyyy!!!!');
+/* jshint esversion: 6*/ //this is module -design pattern object literal implementation of a moduel
+const Counter = {
+  rootElement: '#app',
+  count: 0,
+  start: function(){  //init
+    this.cacheDOM();  //faster - don't want to search dom each time, need something to bind events to
+    this.bindEvents();
+    this.render();
+  }, //kick ff appcac
+  cacheDOM: function(){
+    this.root= document.querySelector('#app');
+    this.button = document.querySelector('#increment');
+    this.output = document.querySelector('#output');
+
+  },//cache what you know won't change, pefrom related
+  bindEvents: function(){
+    //only click events and only on the button
+    this.button.addEventListener('click',()=>{
+    //console.log('click!!!!');
+    this.count += 1;
+    this.render();
+    });
+  },
+
+  //take data pass it thru dom, comesout in html
+  render: function(){
+    this.output.textContent = this.count;
+  }
+};
+Counter.start(); //don't want to forget
